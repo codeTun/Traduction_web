5579e93ef555d661d371530ac26eefeb3b77b360
codeTun <iheblazhary@gmail.com>
2023-12-09 00:32:59 +0100

    (1.0.1) -Fixed some errors

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,8 +5,8 @@ export default function HomePage() {
   return (
     <Layout>
       <Hero
-        title="Landing template for startups"
-        content="Our landing page template works for all the devices, so you only have to setup it once, and get beautiful results forever."
+        title="Web site for translation"
+        content="Translate a web site 3 languages ."
         illustration={<HeroIllustration />}
       />
     </Layout>
